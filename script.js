2e0e5540d3583e6a63d86188ed9323c4e9481adb
BANANA0823 <konno220823@gmail.com>
2026-08-03 23:46:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+window.onload = function () {
+    const saved = localStorage.getItem("kakeibo");
+
+    if (saved) {
+        document.getElementById("list").innerHTML = saved;
+    }
+};
 function add() {
 
     const date = document.getElementById("date").value;
