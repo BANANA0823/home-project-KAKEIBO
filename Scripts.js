026b94a0bd3b5c04062a52d92efd38f28b43c25c
BANANA0823 <konno220823@gmail.com>
2026-08-03 22:59:32 +0900

    Create Scripts.js

---
+++ Scripts.js
@@ -0,0 +1,22 @@
+function add(){
+
+    const date=document.getElementById("date").value;
+    const item=document.getElementById("item").value;
+    const price=document.getElementById("price").value;
+    const category=document.getElementById("category").value;
+
+    if(!date||!item||!price)return;
+
+    const li=document.createElement("li");
+
+    li.innerHTML=
+    `<b>${item}</b><br>
+    ${category}　
+    ¥${Number(price).toLocaleString()}<br>
+    ${date}`;
+
+    document.getElementById("list").appendChild(li);
+
+    document.getElementById("item").value="";
+    document.getElementById("price").value="";
+}
